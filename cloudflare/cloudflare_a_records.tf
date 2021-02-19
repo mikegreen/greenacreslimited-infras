da90de854e92fd391aa621812568a9df57ae910b
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-02-19 11:45:27 -0700

    cleanup

---
+++ cloudflare/cloudflare_a_records.tf
@@ -0,0 +1,26 @@
+resource "cloudflare_record" "A_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = "greenacreslimited.com"
+  type    = "A"
+  ttl     = "1"
+  proxied = "true"
+  value   = "69.163.155.199"
+}
+
+resource "cloudflare_record" "A_vault_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = "vault"
+  type    = "A"
+  ttl     = "1"
+  proxied = "false"
+  value   = "75.166.39.232"
+}
+
+resource "cloudflare_record" "A_vault-pr_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = "vault-pr"
+  type    = "A"
+  ttl     = "1"
+  proxied = "true"
+  value   = "18.223.119.112"
+}
