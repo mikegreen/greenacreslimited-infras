d418b3792d408b586ef3fad8ede8e71d2833b2b7
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-02-19 11:27:45 -0700

    initial records

---
+++ cloudflare/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "cloudflare" { 
+}
+
+
+resource "cloudflare_zone" "greenacreslimited" {
+  zone = "greenacreslimited.com"
+  type = "full"
+}
+
+resource "cloudflare_record" "A_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = "greenacreslimited.com"
+  type    = "A"
+  ttl     = "1"
+  proxied = "true"
+  value   = "69.163.155.199"
+}
+
+resource "cloudflare_record" "A_vault_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = "vault"
+  type    = "A"
+  ttl     = "1"
+  proxied = "false"
+  value   = "75.166.39.232"
+}
