4bcae276ed9e86a60e6d46055b09585436a52437
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-04-20 11:23:33 -0600

    Update cloudflare_txt_records.tf

--- cloudflare/cloudflare_txt_records.tf
+++ cloudflare/cloudflare_txt_records.tf
@@ -15,3 +15,12 @@ resource "cloudflare_record" "TXT_google_verif_greenacreslimited" {
   proxied = "false"
   value   = "google-site-verification=uP0NdVMRRbaJGmtlGUyDHeNnA9aDBBYDwq5sE2uUCpA"
 }
+
+resource "cloudflare_record" "TXT_acme_vault_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = "_acme-challenge.vault"
+  type    = "TXT"
+  ttl     = "1"
+  proxied = "false"
+  value   = "VZcaQrviQwYur9Jhf_toUoXcU5rS9QVhQtpnUn8DBek"
+}
