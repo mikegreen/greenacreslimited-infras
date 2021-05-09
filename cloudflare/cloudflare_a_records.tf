5d25c32a01cbf765a860067e639d2d452509a029
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-05-09 10:07:43 -0600

    Update cloudflare_a_records.tf

--- cloudflare/cloudflare_a_records.tf
+++ cloudflare/cloudflare_a_records.tf
@@ -22,7 +22,7 @@ resource "cloudflare_record" "A_vault_greenacreslimited" {
   type    = "A"
   ttl     = "1"
   proxied = "false"
-  value   = "75.166.40.0"
+  value   = "75.166.62.620"
 }
 
 resource "cloudflare_record" "A_vault-pr_greenacreslimited" {
