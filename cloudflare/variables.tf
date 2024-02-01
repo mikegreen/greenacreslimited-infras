49223ff345854b52f53aa18b1ab99efd81648efd
Mike Green <772413+mikegreen@users.noreply.github.com>
2024-02-01 09:59:41 -0700

    clenup and comment

--- cloudflare/variables.tf
+++ cloudflare/variables.tf
@@ -1,16 +1,16 @@
 # variables.tf
 
 variable "cl_dynamic_ip" {
-	type = string
-	description = "Dynamic Centurylink IP that changes"
+  type        = string
+  description = "Dynamic Centurylink IP that changes"
 }
 
 variable "cloudflare_account_id" {
-	type = string
-	description = "Cloudflare account ID reqd as of 4.0"
+  type        = string
+  description = "Cloudflare account ID reqd as of 4.0"
 }
 
 variable "cloudflare_api_token" {
-	type = string
-	description = "Cloudflare API token (not key!)"
+  type        = string
+  description = "Cloudflare API token (not key!)"
 }
