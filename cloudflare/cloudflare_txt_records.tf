434d367c9a94f91e64c6ae7af003f5524a6602ff
Mike Green <772413+mikegreen@users.noreply.github.com>
2024-04-04 13:08:47 -0600

    add microsoft txt verif

--- cloudflare/cloudflare_txt_records.tf
+++ cloudflare/cloudflare_txt_records.tf
@@ -24,3 +24,12 @@ resource "cloudflare_record" "TXT_acme_vault_greenacreslimited" {
   proxied = "false"
   value   = "VZcaQrviQwYur9Jhf_toUoXcU5rS9QVhQtpnUn8DBek"
 }
+
+resource "cloudflare_record" "TXT_microsoft_verif_greenacreslimited" {
+  zone_id = cloudflare_zone.greenacreslimited.id
+  name    = cloudflare_zone.greenacreslimited.zone
+  type    = "TXT"
+  ttl     = "1"
+  proxied = "false"
+  value   = "MS=EE741130AA91BE33BB396C1C3FE97A128B573071"
+}
